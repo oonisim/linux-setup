d0f4344526f1f0c5584846275f9431795f55f7b5
oonisim <proxymio@gmail.com>
2018-10-31 16:01:49 +1000

    Initial

--- scripts/ansible/linux/03_packages/scripts/setup_roles.sh
+++ scripts/ansible/linux/03_packages/scripts/setup_roles.sh
@@ -12,3 +12,7 @@ for role in "${roles[@]}"
 do
     ansible-galaxy install --roles-path ${_ROLE_DIR} ${role}
 done
+
+
+mkdir -p oefenweb_r              && curl -L https://github.com/Oefenweb/ansible-r/tarball/master              | tar -xz --overwrite --strip 1 -C oefenweb_r
+mkdir -p oefenweb_rstudio_server && curl -L https://github.com/Oefenweb/ansible-rstudio-server/tarball/master | tar -xz --overwrite --strip 1 -C oefenweb_rstudio_server
