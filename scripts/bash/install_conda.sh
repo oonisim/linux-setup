8bb3db2d3362d86d36deaf656bc52a2d909e5bd2
oonisim <oonisim@gmail.com>
2021-06-18 20:33:27 -0700

    Added Anaconda installation script. Enabled chrome installation

--- scripts/bash/install_conda.sh
+++ scripts/bash/install_conda.sh
@@ -26,8 +26,8 @@ __conda_setup="$('/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/opt/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/conda/etc/profile.d/conda.sh" ]; then
+        . "$HOME/conda/etc/profile.d/conda.sh"
     else
         export PATH="$HOME/conda/bin:$PATH"
     fi
